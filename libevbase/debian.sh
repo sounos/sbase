5e2613a3d8c363e7e229f6599c984f802dcbee90
root <sounos@qq.com>
2014-06-10 16:57:43 +0800

    fixed debian.sh

--- libevbase/debian.sh
+++ libevbase/debian.sh
@@ -8,4 +8,4 @@ sed -i "s/^Source:.*/Source: $pkg/" debian/control
 sed -i "s/^License:.*/License: $pkg/" debian/control
 sed -i "s/^Standards-Version:.*/Standards-Version: $ver/" debian/control
 sed -i "s/root <SounOS@gmail.com>/SounOS <SounOS@gmail.com>/" debian/*
-dpkg-buildpackage -rfakeroot -eSounOS -mSounOS
+dpkg-buildpackage -d -rfakeroot -eSounOS -mSounOS
